58f7245eb004dfcc20ddda702dfadab4c90fb0fe
Josexv1 <jozhee@live.com>
2015-09-08 13:41:40 -0430

    Add test para chequear si tor esta encendido.

--- KaliMeUp.py
+++ KaliMeUp.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import os
 # Colores de consola
 #W  = '\033[0m'  # white (normal)
 #R  = '\033[31m' # red
@@ -9,17 +8,22 @@ import os
 #P  = '\033[35m' # purple
 #C  = '\033[36m' # cyan
 #GR = '\033[37m' # gray
+import os
 import sys, traceback, platform, time
-#if platform.linux_distribution() == ('Sana', '', ''): #Will Fix this soon.
+import urllib2
+from urlparse import urlparse
+#No usable aun.
+# if platform.linux_distribution() == ('Sana', '', ''): #Will Fix this soon.
 #   main()
-#else: 
-#	print 'Parece que no estas usando Kali Linux 2.0'
-#	print 'No podemos dejarte usar este programa'
+# else: 
+# 	print 'Parece que no estas usando Kali Linux 2.0'
+# 	print 'No podemos dejarte usar este programa'
+
 #Pedimos ser ROOT
-# if os.getuid() != 0:
-# 	print '\033[31m[!]\033[33m ERROR: \033[36m KaliMeUp \033[33m debe correr como\033[34m root\033[0m'
-# 	print '\033[31m[!]\033[33m Inicia sesion como root \033[34m(sudo su)\033[0m o \033[34m sudo kalimeup \033[0mo \033[34msudo python kalimeup.py\033[0m'
-# 	exit(1)
+if os.getuid() != 0:
+	print '\033[31m[!]\033[33m ERROR: \033[36m KaliMeUp \033[33m debe correr como\033[34m root\033[0m'
+	print '\033[31m[!]\033[33m Inicia sesion como root \033[34m(sudo su)\033[0m o \033[34m sudo kalimeup \033[0mo \033[34msudo python kalimeup.py\033[0m'
+	exit(1)
 def main():
 	try:
 		def banner():
@@ -105,6 +109,7 @@ Instalador Automatico para Kali linux 2.0 Sana
 						menu()
 					else:
 						print ("Comando invalido")
+						time.sleep(0.5)
 					# Fin Opciones de repositorios!
 				#Opcion 2 del menu principal	
 				while opcion0 == "2":
@@ -155,7 +160,7 @@ Instalador Automatico para Kali linux 2.0 Sana
 						time.sleep(2)
 					elif herr == "10":
 						print '[-] BETA!'
-						opc = raw_input('Arquitectura? 32/64 bits\n')
+						opc = raw_input('Arquitectura? 32/64 bits > ')
 						if opc == "32":
 							print 'Instalaremos Dropbox 32 bits se paciente.\n'
 							time.sleep(2)
@@ -172,7 +177,7 @@ Instalador Automatico para Kali linux 2.0 Sana
 						time.sleep(2)
 					elif herr == "12":
 						print '[-] BETA!'
-						opc = raw_input('Arquitectura? 32/64 bits\n')
+						opc = raw_input('Arquitectura? 32/64 bits > ')
 						if opc == "32":
 							print 'Instalaremos Sublime text 3 32 bits se paciente.\n'
 							time.sleep(2)
@@ -195,33 +200,112 @@ Instalador Automatico para Kali linux 2.0 Sana
 				while opcion0 == "3":
 						banner();
 						print '''
-	### Las herramientas Rolling hacen de Kali Linux
-	### una version con Apps de uso diario
+### Herramientas de Hacking extras
+### Coleccion de herramientas necesarias
 
-	1) Instalar  8)  Instalar 
-	2) Instalar  9)  Instalar 
-	3) Instalar  10) Instalar 
-	4) Instalar  11) Instalar 
-	5) Instalar  12) Instalar 
-	6) Instalar  13) Instalar 
-	7) Instalar 
+1) Instalar  8)  Instalar 
+2) Instalar  9)  Instalar 
+3) Instalar  10) Instalar 
+4) Instalar  11) Instalar 
+5) Instalar  12) Instalar 
+6) Instalar  13) Instalar 
+7) Instalar 
 
-	99) Atras 
-
+99) Atras 
 						'''
-						herr = raw_input("Usar ?>")
+						herr = raw_input("Usar ?> ")
 						if herr == "1":
 							print 'xd'
 						elif herr == "2":
 							print 'xd'
+						elif herr == "99":
+							menu()
 						else:
 							print ("Comando invalido")
+							time.sleep(0.3)
+				while opcion0 == "4":
+						banner();
+						print '''
+Backbox-Anonymous enruta todo tu trafico por TOR.
+Haciendo que cada conexion de cualquier programa use TOR para ocultar tu IP 
+Y mejorar tu privacidad.
+
+Puedes ver como instalarlo manualmente en 
+https://github.com/Josexv1/Kali-Anonymous
+
+1) Instalar Backbox-Anonymous
+2) Probar conexion a Tor!
+99) Atras 
+						'''
+						herr = raw_input("Usar ?> ")
+						if herr == "1":
+							os.system('git clone https://github.com/Josexv1/Kali-Anonymous.git')
+							time.sleep(0.5)
+							os.system('cp Kali-Anonymous/etc/default/backbox-anonymous /etc/default/')
+							time.sleep(0.1)
+							os.system('cp Kali-Anonymous/usr/bin/backbox-anonymous /usr/bin/')
+							time.sleep(0.1)
+							print 'Se ha instalado Backbox-Anonymous'
+							print 'Vamos a iniciar TOR'
+							time.sleep(0.5)
+							os.system('service tor start')
+							print 'Debemos esperar unos 5 segundos para que se abra la conexion correctamente'
+							time.sleep(5)
+							print 'Tor esta corriendo.'
+							time.sleep(1)
+							os.system('backbox-anonymous start')
+							print 'Ahora todo tu trafico corre bajo TOR!'
+							time.sleep(5)
+						elif herr == "2":
+							# Intento de probar conexion con TOR!
+							try:
+								print 'Vamos a probar tu conexion a Tor, si acabas de encender el demonio'
+								print 'debes esperar unos 10 segundos a que se conecte bien la red Tor'
+								time.sleep(5)
+								print("\nEnviando una peticion a : https://check.torproject.org\n")
+								resp_tor = urllib2.urlopen("https://check.torproject.org").read()
+								tu_ip = resp_tor.split('<strong>')[1].split('</strong>')[0].strip()
+								if not resp_tor or 'Congratulations' not in resp_tor:
+								    print("Parece que Tor no se inicio bien.\n")
+								    print("Tu IP parece ser: " + tu_ip + "\n")
+								    time.sleep(3)
+								else:
+								    print("Felicitaciones! Ahora tu trafico pasa por Tor! :-)\n")
+								    print("Tu IP parece ser: " + tu_ip + "\n")
+								    time.sleep(3)
+							except:
+							    print("No pudomos llegar al sistema de pruebas!. Tienes Internet?\n")
+							    time.sleep(3)
+						elif herr == "99":
+							menu()
+						else:
+							print ("Comando invalido")
+							time.sleep(0.3)
+				while opcion0 == "5":
+						banner();
+						print '''
+Kali Me Up, es un software para instalar automaticamente muchas herramientas
+Puedes instalar Firefox, Tor, Backbox-Anonymous y otras herramientas
+Es Opensource y Puedes ver el codigo aqui:
+https://github.com/Josexv1/KaliMeUp
+Poco a poco se agregaran herramientas que no vienen incluidas en los repos
+de Kali Linux oficiales, cuyo contenido esta en GitHub.
+
+Puedes poner issues y/o Pull requests en el github.
+
+99) Atras 
+						'''
+						opc2 = raw_input("Usar ?> ")
+						if opc2 == "99":
+							menu()
+						else:
+							print 'Comando invalido'
 							time.sleep(0.3)
 			menu()
 		menu()	
 	except KeyboardInterrupt:
 	  os.system('clear')
-	  print "Shutdown requested...Goodbye..."
+	  print "Apagando el programa."
 	except Exception:
 	  traceback.print_exc(file=sys.stdout)
 	sys.exit(0)
